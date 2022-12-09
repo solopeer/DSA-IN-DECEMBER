68b11053176bfd32a7e578d6b74f430544ffe296
Shrawan Saproo <102973966+avsk3234@users.noreply.github.com>
2022-12-10 01:07:54 +0530

    Add files via upload

---
+++ DAY-5/icecream-parlour.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    m = int(input())
+    n = int(input())
+    cost = list(map(int, input().split()))
+    memo = {}
+    for i, c in enumerate(cost):
+        if (m - c) in memo:
+            print('%s %s' % (memo[m - c], i + 1))
+            break
+        memo[c] = i + 1
+
+
