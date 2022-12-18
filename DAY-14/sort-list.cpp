77c84ca3ca4dccea686eb0d15a80a6f2c0daa4a6
Shrawan Saproo <102973966+avsk3234@users.noreply.github.com>
2022-12-18 23:14:35 +0530

    Create sort-list.cpp

---
+++ DAY-14/sort-list.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if(!head) return head;
+        vector<int> arr;
+        ListNode* cur = head;
+        
+        while(cur){
+            arr.push_back(cur->val);
+            cur = cur->next;
+        }
+        
+        sort(arr.begin(), arr.end());
+        
+        cur = head;
+        int i = 0;
+        while(cur){
+            cur->val = arr[i++];
+            cur = cur->next;
+        }
+        
+        return head;
+    }
+};
+
+class Solution {
+public:
+    ListNode* merge(ListNode* a, ListNode* b){
+        ListNode *newHead = new ListNode(0);
+        ListNode *cur = newHead;
+        cur = a;
+        while(cur){
+            cur = cur->next;
+        }
+        cur = newHead;
+        while(a && b){
+            if(a->val <= b->val){
+
+                cur->next = a;
+                a = a->next;
+            }else{
+                cur->next = b;
+                b = b->next;
+            }
+            cur = cur->next;
+        }
+
+        if(a){
+            cur->next = a;
+        }else if(b){
+            cur->next = b;
+        }
+
+        return newHead->next;
+    };
+    
+    ListNode* sortList(ListNode* head) {
+   
+        if(!head || !head->next) return head;
+
+        ListNode* cur = head;
+        while(cur){
+            cur = cur->next;
+        }
+        ListNode *slow = head, *fast = head;
+        ListNode *slowPrev; 
+        
+        while(fast && fast->next){
+            slowPrev = slow;
+            
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        slowPrev->next = nullptr;
+        
+        head = sortList(head);
+        slow = sortList(slow);
+        
+        return merge(head, slow);
+    }
+};
