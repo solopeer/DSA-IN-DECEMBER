05b96da8824f441efc61ecc5059de1b1120dd816
Shrawan Saproo <102973966+avsk3234@users.noreply.github.com>
2023-01-10 03:08:43 +0530

    Create classes-objects.py

---
+++ DAY-15/classes-objects.py
@@ -0,0 +1,13 @@
+# define a class
+class Bike:
+    name = ""
+    gear = 0
+
+# create object of class
+bike1 = Bike()
+
+# access attributes and assign new values
+bike1.gear = 11
+bike1.name = "Mountain Bike"
+
+print(f"Name: {bike1.name}, Gears: {bike1.gear} ")
